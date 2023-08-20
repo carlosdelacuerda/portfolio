40277df8a0e98e17a904afa47afe6f1751e97334
carlosdelacuerda <carlosdelacuerda@gmail.com>
2023-08-20 20:15:51 +0200

    packages added

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -9,67 +9,67 @@ const CarouselData = [
   {
     headerText: 'UI Kankuro',
     subText: null,
-    image: '/images/img01.jpg',
+    image: './images/img01.jpg',
   },
   {
     headerText: 'UX MiDieta',
     subText: null,
-    image: '/images/img02.webp',
+    image: './images/img02.webp',
   },
   {
     headerText: 'UI Ruta Interactiva',
     subText: null,
-    image: '/images/img03.webp',
+    image: './images/img03.webp',
   },
   {
     headerText: 'UX MiDieta',
     subText: null,
-    image: '/images/img04.webp',
+    image: './images/img04.webp',
   },
   {
     headerText: 'UI DStudio',
     subText: null,
-    image: '/images/img06.webp',
+    image: './images/img06.webp',
   },
   {
     headerText: 'UI Drive Smart',
     subText: null,
-    image: '/images/img05.webp',
+    image: './images/img05.webp',
   },
   {
     headerText: 'UI DStudio',
     subText: null,
-    image: '/images/img07.webp',
+    image: './images/img07.webp',
   },
   {
     headerText: 'UI Home Finder',
     subText: null,
-    image: '/images/img08.webp',
+    image: './images/img08.webp',
   },
   {
     headerText: 'UI Take a walk',
     subText: null,
-    image: '/images/img09.webp',
+    image: './images/img09.webp',
   },
   {
     headerText: 'UI Pedagogía y salud',
     subText: null,
-    image: '/images/img10.webp',
+    image: './images/img10.webp',
   },
   {
     headerText: 'UI Somos Corazón',
     subText: null,
-    image: '/images/img11.webp',
+    image: './images/img11.webp',
   },
   {
     headerText: 'UI DriveSmart',
     subText: null,
-    image: '/images/img12.webp',
+    image: './images/img12.webp',
   },
   {
     headerText: 'UI WeAlert',
     subText: null,
-    image: '/images/img13.webp',
+    image: './images/img13.webp',
   }
 ]
   return (   
