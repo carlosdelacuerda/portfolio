0c601c9c0de0547af74c4482ee2694e6b3f91bc5
carlosdelacuerda <carlosdelacuerda@gmail.com>
2023-08-20 19:07:35 +0200

    first deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from '@mui/material/Container';
+import ResponsiveAppBar from './components/header.tsx';
+import AppCarousel from './components/carousel';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import AboutApp from './components/about.tsx';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="xl">
+        <ResponsiveAppBar></ResponsiveAppBar>
+          <Router>
+            <Routes>
+              <Route exact path="/" element={<AppCarousel />}>
+              </Route>
+              <Route exact path="about" element={ <AboutApp />}>
+              </Route>
+            </Routes>
+          </Router>
+      </Container>
     </div>
-  );
+ );
 }
 
 export default App;
---
+++ src/components/carousel.jsx
@@ -0,0 +1,92 @@
+import { Carousel } from '@sefailyasoz/react-carousel'
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
+import Container from '@mui/material/Container';
+
+
+const AppCarousel = () => {
+
+const CarouselData = [
+  {
+    headerText: 'UI Kankuro',
+    subText: null,
+    image: '/images/img01.jpg',
+  },
+  {
+    headerText: 'UX MiDieta',
+    subText: null,
+    image: '/images/img02.webp',
+  },
+  {
+    headerText: 'UI Ruta Interactiva',
+    subText: null,
+    image: '/images/img03.webp',
+  },
+  {
+    headerText: 'UX MiDieta',
+    subText: null,
+    image: '/images/img04.webp',
+  },
+  {
+    headerText: 'UI DStudio',
+    subText: null,
+    image: '/images/img06.webp',
+  },
+  {
+    headerText: 'UI Drive Smart',
+    subText: null,
+    image: '/images/img05.webp',
+  },
+  {
+    headerText: 'UI DStudio',
+    subText: null,
+    image: '/images/img07.webp',
+  },
+  {
+    headerText: 'UI Home Finder',
+    subText: null,
+    image: '/images/img08.webp',
+  },
+  {
+    headerText: 'UI Take a walk',
+    subText: null,
+    image: '/images/img09.webp',
+  },
+  {
+    headerText: 'UI Pedagogía y salud',
+    subText: null,
+    image: '/images/img10.webp',
+  },
+  {
+    headerText: 'UI Somos Corazón',
+    subText: null,
+    image: '/images/img11.webp',
+  },
+  {
+    headerText: 'UI DriveSmart',
+    subText: null,
+    image: '/images/img12.webp',
+  },
+  {
+    headerText: 'UI WeAlert',
+    subText: null,
+    image: '/images/img13.webp',
+  }
+]
+  return (   
+  <Container>
+    <h1 className='title-section'>Portfolio</h1>
+    <Carousel
+                data={CarouselData}
+                autoPlay={true}
+                rightItem={<FaArrowRight />}
+                leftItem={<FaArrowLeft />}
+                animationDuration={3000}
+                headerTextType="black"
+                subTextType="white"
+                size="normal"
+              />
+  </Container>
+  )
+}
+
+export default AppCarousel
